b5f12c9314992fe1d21dc5bb149625ad794710a9
Kerim <khersonskiy.kerim@gmail.com>
2024-04-12 17:11:00 +0200

    Дз 6.

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Model;
 using Model.Runtime.Projectiles;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
@@ -12,52 +18,71 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
+        
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
-            float overheatTemperature = OverheatTemperature;
-                ///////////////////////////////////////
-                IncreaseTemperature();
-                ///////////////////////////////////////
+            
+
+                IncreaseTemperature();       
+            
                 for(int i=0; i< GetTemperature() && GetTemperature() < OverheatTemperature ; i++)
                 {
                     var projectile = CreateProjectile(forTarget);
                     AddProjectileToList(projectile, intoList);
 
                 }
-                ///////////////////////////////////////
         }
-
+        
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            Vector2Int Target = new Vector2Int();
+            Target = SelectTargets()[0];
+            //4. В методе GetNextStep() нужно описать получение цели из списка целей. Если целей там нет или цель в области атаки, нужно вернуть позицию юнита.
+            if (IsTargetInRange(Target)) 
+            { 
+                return unit.Pos;
+
+            }
+            //5. Если цель есть, но вне области атаки, в GetNextStep() вызвать у текущей позиции метод CalcNextStepTowards(), передав туда цель. 
+            else 
+            {
+                return unit.Pos.CalcNextStepTowards(Target); 
+
+            }
+
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
-            ///////////////////////////////////////
+            //1. Вместо достижимых целей получи все с помощью метода GetAllTargets().
+            var AllTargets = GetAllTargets();
 
-            ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            Debug.Log(result);
+            //2a. Создаем новое поле для хранения целей, к которым нужно идти, но которые вне зоны досягаемости.
+            List<Vector2Int> NoReachableTargets = new List<Vector2Int>();
+
+            //2b. Записываем самую опасную цель в эту коллекцию. Если цель в зоне досягаемости, то добавляем в result.
+            List<Vector2Int> result = new List<Vector2Int>();
             float max = float.MaxValue;
-            Vector2Int nearest = Vector2Int.zero;
-            while (result.Count > 1)
+            var nearest = new Vector2Int();
+            result.Clear();
+            foreach (var target in AllTargets)
             {
-                foreach (var i in result)
+                if (DistanceToOwnBase(target) < max)
                 {
-                    float j = DistanceToOwnBase(i);
-
-                    if (j < max)
-                    {
-                        max = j;
-                        nearest= i;
-                    }
+                    max = DistanceToOwnBase(target);
+                    nearest=target;
                 }
+            }
                 result.Clear();
                 result.Add(nearest);
+            
+            //3. Если целей нет, добавляем в цели базу противника.
+            if (result.Count==0)
+            {
+                result.Clear();
+                result.Add(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId]);
             }
             return result;
-            ///////////////////////////////////////
         }
 
         public override void Update(float deltaTime, float time)
