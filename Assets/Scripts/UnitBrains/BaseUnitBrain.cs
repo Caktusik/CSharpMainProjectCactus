9a6e29d13bd964afa9320c9e97e8bf6ab38256c3
Kerim <khersonskiy.kerim@gmail.com>
2024-07-01 21:20:48 +0200

    ДЗ 13

--- Assets/Scripts/UnitBrains/BaseUnitBrain.cs
+++ Assets/Scripts/UnitBrains/BaseUnitBrain.cs
@@ -15,10 +15,11 @@ namespace UnitBrains
     {
         public virtual string TargetUnitName => string.Empty;
         public virtual bool IsPlayerUnitBrain => true;
+        //public virtual bool IsPlayerSupportUnitBrain => false;
         public virtual BaseUnitPath ActivePath => _activePath;
 
-        protected UnitsCoordinator coordinator;
         protected Unit unit { get; private set; }
+        protected UnitsCoordinator coordinator;
         protected IReadOnlyRuntimeModel runtimeModel => ServiceLocator.Get<IReadOnlyRuntimeModel>();
         private BaseUnitPath _activePath = null;
 
@@ -40,45 +41,9 @@ namespace UnitBrains
 
             var target = runtimeModel.RoMap.Bases[
                 IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId];
-            if (!IsPlayerUnitBrain)
-            {
-                var diff = target - unit.Pos;
-                var stepDiff = diff.SignOrZero();
-                var nextStep = unit.Pos + stepDiff;
-
-                if (runtimeModel.IsTileWalkable(nextStep))
-                    return nextStep;
-
-                if (stepDiff.sqrMagnitude > 1)
-                {
-                    var partStep0 = unit.Pos + new Vector2Int(stepDiff.x, 0);
-                    if (runtimeModel.IsTileWalkable(partStep0))
-                        return partStep0;
-
-                    var partStep1 = unit.Pos + new Vector2Int(0, stepDiff.y);
-                    if (runtimeModel.IsTileWalkable(partStep1))
-                        return partStep1;
-                }
-
-                var sideStep0 = unit.Pos + new Vector2Int(stepDiff.y, -stepDiff.x);
-                var shiftedStep0 = unit.Pos + (sideStep0 + stepDiff).SignOrZero();
-                if (runtimeModel.IsTileWalkable(shiftedStep0))
-                    return shiftedStep0;
-
-                var sideStep1 = unit.Pos + new Vector2Int(-stepDiff.y, stepDiff.x);
-                var shiftedStep1 = unit.Pos + (sideStep1 + stepDiff).SignOrZero();
-                if (runtimeModel.IsTileWalkable(shiftedStep1))
-                    return shiftedStep1;
-
-                if (runtimeModel.IsTileWalkable(sideStep0))
-                    return sideStep0;
-
-                if (runtimeModel.IsTileWalkable(sideStep1))
-                    return sideStep1;
-
-                return unit.Pos;
-            }
+            //Debug.Log("target" + target);
             _activePath = new AStarUnitPath(runtimeModel, unit.Pos, target);
+            //Debug.Log("BaseUnitBrain.GetNextStep():" + _activePath.GetNextStepFrom(unit.Pos));
             return _activePath.GetNextStepFrom(unit.Pos);
         }
 
@@ -102,6 +67,11 @@ namespace UnitBrains
         public void SetUnit(Unit unit)
         {
             this.unit = unit;
+        }
+
+        public void SetCoordinator(UnitsCoordinator coordinator)
+        {
+            this.coordinator = coordinator;
         }
 
         public virtual void Update(float deltaTime, float time)
@@ -173,12 +143,6 @@ namespace UnitBrains
                 .Where(u => u.Config.IsPlayerUnit != IsPlayerUnitBrain);
         }
 
-        public void SetCoordinator(UnitsCoordinator coordinator)
-        {
-            this.coordinator = coordinator;
-        }
-
-
         protected IEnumerable<Vector2Int> GetAllTargets()
         {
             return runtimeModel.RoUnits
@@ -205,7 +169,6 @@ namespace UnitBrains
 
                 result.Add(possibleTarget);
             }
-
             return result;
         }
     }
--- Assets/Scripts/UnitBrains/Player/DefaultPlayerUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/DefaultPlayerUnitBrain.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using Assets.Scripts.UnitBrains.Player;
 using Model;
 using Model.Runtime.ReadOnly;
 using UnitBrains.Pathfinding;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
     public class DefaultPlayerUnitBrain : BaseUnitBrain
     {
         protected UnitsCoordinator unitsCoordinator = new UnitsCoordinator(RuntimeModel.PlayerId);
+        protected IReadOnlyRuntimeModel _runtimeModel => ServiceLocator.Get<IReadOnlyRuntimeModel>();
         protected float DistanceToOwnBase(Vector2Int fromPos) =>
             Vector2Int.Distance(fromPos, runtimeModel.RoMap.Bases[RuntimeModel.PlayerId]);
 
@@ -27,20 +30,41 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-
             Vector2Int recommendedTarget = unitsCoordinator.recommendedTarget;
             Vector2Int recommendedPoint = unitsCoordinator.recommendedPoint;
-
             AStarUnitPath path = new AStarUnitPath(runtimeModel, unit.Pos, recommendedPoint);
-            if (HasTargetsInRange())
+
+            if (IsTargetInVisionRange(recommendedTarget))
             {
                 if (IsTargetInRange(recommendedTarget))
                 {
                     return unit.Pos;
                 }
-                return path.GetNextStepFrom(unit.Pos);
+                path = new AStarUnitPath(runtimeModel, unit.Pos, recommendedTarget);
             }
-            return base.GetNextStep();
+            return path.GetNextStepFrom(unit.Pos);
+        }
+
+        protected override List<Vector2Int> SelectTargets()
+        {
+            var result = new List<Vector2Int>();
+
+            var targets = GetReachableTargets();
+            var recommendedTarget = unitsCoordinator.recommendedTarget;
+
+            if (targets.Contains(recommendedTarget))
+                result.Add(recommendedTarget);
+
+            return result;
+        }
+
+
+
+        private bool IsTargetInVisionRange(Vector2Int targetPos)
+        {
+            float visionRangeSqr = unit.Config.AttackRange * unit.Config.AttackRange * 2;
+            var diff = targetPos - unit.Pos;
+            return diff.sqrMagnitude <= visionRangeSqr;
         }
     }
 }
